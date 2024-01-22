559c83b2a1adbec155fb5a6533e076024c2ef6b8
Chris Lai <chrisyflai@gmail.com>
2024-01-22 10:05:51 +0000

    change

--- pages/Home/jsobjects/functions/functions.js
+++ pages/Home/jsobjects/functions/functions.js
@@ -3,11 +3,11 @@ export default {
 	setActiveUser: (user) => {
 		this.activeUser = user;
 	},
-	getUserFromToken: async () => {
-		const token = appsmith.store.token;
-		const user = jsonwebtoken.decode(token, 'secret');
-		this.setActiveUser(user);
-	},
+	// getUserFromToken: async () => {
+		// const token = appsmith.store.token;
+		// const user = jsonwebtoken.decode(token, 'secret');
+		// this.setActiveUser(user);
+	// },
 	signOut: async () => {
 		clearStore('token', null).then(() => navigateTo('Authentication'));
 		showAlert('Logout Success!', 'success');
