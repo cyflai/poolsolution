07d48329da69c2b318b3a1a584982efb1eceb2df
Chris Lai <chrisyflai@gmail.com>
2024-01-25 06:02:20 +0000

    update

--- pages/Login/jsobjects/auth/auth.js
+++ pages/Login/jsobjects/auth/auth.js
@@ -1,14 +1,20 @@
 export default {
+	supabaseUrl: 'https://afhvocspeeserasytprs.supabase.co',
+	supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmaHZvY3NwZWVzZXJhc3l0cHJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU4MjI0OTEsImV4cCI6MjAyMTM5ODQ5MX0.s0FtT89QeWCxTf8szuAtAyu8us0AZ5dhk6i13fMyvp8',
+	sb: supabase.createClient(this.supabaseUrl,this.supabaseKey),
 	signIn: async () => {
 		try{
 				await sign_in.run().then(data =>{
 
 					storeValue("email",data.user.email)
+					storeValue('username',inp_Username.text)
 					delete data.user;
 
 					Object.keys(data).forEach(i => {
 						storeValue(i, data[i]);
 					});
+					
+					
 
 				}).then(()=> navigateTo('Profile'))
 			
@@ -19,9 +25,18 @@ export default {
 	},
 	signUp: async () => {
 		try{
-				await sign_up.run();
+			await sign_up.run();
 			
 			showAlert("Account created successfully",'success')
+			
+			const { data, error } = await this.sb
+				.from('userprofiles')
+				.insert([
+				{ email: inp_registerUsername.text + '@obr.com.hk', username: inp_registerUsername.text },
+			])
+			.select()
+			
+			console.log(data)
 			
 			functions.setDefaultTab('Sign In')
 	
--- pages/Profile/jsobjects/functions/functions.js
+++ pages/Profile/jsobjects/functions/functions.js
@@ -1,4 +1,7 @@
 export default {
+	supabaseUrl: 'https://afhvocspeeserasytprs.supabase.co',
+	supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmaHZvY3NwZWVzZXJhc3l0cHJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU4MjI0OTEsImV4cCI6MjAyMTM5ODQ5MX0.s0FtT89QeWCxTf8szuAtAyu8us0AZ5dhk6i13fMyvp8',
+	sb: supabase.createClient(this.supabaseUrl,this.supabaseKey),
 	checkToken: () => {
 		
 			if (!appsmith.store.access_token) {
@@ -7,9 +10,35 @@ export default {
 			return appsmith.store.access_token		
 			}
 		
-	},	
-	getUserProfile:()=>{
-		
-		
-	}
+	},
+	updateWalletAddr: async () => {
+		try{
+			if (inpAddress.text == "") {
+				showAlert("Wallet ddress can't be empty, please input your Wallet address and update again", 'error')
+				return
+			} 
+			await this.sb
+			.from('userprofiles')
+			.update({ wallet_address: inpAddress.text })
+			.eq('username', appsmith.store.username)
+			await btnAdd.setDisabled(true)
+		  showAlert("Wallet Address successfully updated",'success')
+     }catch(error){
+			showAlert("Failed to update Wallet Address, Please try again later",'error')
+			console.log(error.message)
+     }
+	},
+	enableUpdateBtn: async ()=>{
+		await btnAdd.setDisabled(false)
+	},
+		disableUpdateBtn: async ()=>{
+		await btnAdd.setDisabled(true)
+	},
+	// startup: async () => {
+		// if ( inpAddress.text == "") {
+			// btnAdd.setLabel("Add")
+		// } else {
+			// btnAdd.setLabel("Edit")
+		// }
+	// }
 }
