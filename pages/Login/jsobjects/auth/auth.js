a648534be934e3db6bd71473f5b9012fb8dfb272
Chris Lai <chrisyflai@gmail.com>
2024-01-26 02:17:01 +0000

    update

--- pages/Login/jsobjects/auth/auth.js
+++ pages/Login/jsobjects/auth/auth.js
@@ -2,41 +2,45 @@ export default {
 	supabaseUrl: 'https://afhvocspeeserasytprs.supabase.co',
 	supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmaHZvY3NwZWVzZXJhc3l0cHJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU4MjI0OTEsImV4cCI6MjAyMTM5ODQ5MX0.s0FtT89QeWCxTf8szuAtAyu8us0AZ5dhk6i13fMyvp8',
 	sb: supabase.createClient(this.supabaseUrl,this.supabaseKey),
+	
 	signIn: async () => {
 		try{
-				await sign_in.run().then(data =>{
-
-					storeValue("email",data.user.email)
-					storeValue('username',inp_Username.text)
-					delete data.user;
-
-					Object.keys(data).forEach(i => {
-						storeValue(i, data[i]);
-					});
+				await sign_in.run().then(async data =>{
 					
+						storeValue("email",data.user.email)
+						storeValue('username',inp_Username.text)
 					
-
-				}).then(()=> navigateTo('Profile'))
-			
-     }catch(e){
+						delete data.user;
+				
+						Object.keys(data).forEach(i => {
+							storeValue(i, data[i]);
+						});
+					
+						storeValue('minerId',await functions.getMinerId(inp_Username.text))
+						
+					}).then(
+					// navigateTo('Profile')	
+				)
+			appsmith.store
+     } catch(e) {
 				showAlert(sign_in.data.error_description,'error')
      }
-
 	},
+
 	signUp: async () => {
 		try{
 			await sign_up.run();
 			
 			showAlert("Account created successfully",'success')
 			
-			const { data, error } = await this.sb
-				.from('userprofiles')
-				.insert([
-				{ email: inp_registerUsername.text + '@obr.com.hk', username: inp_registerUsername.text },
-			])
-			.select()
+				const { data, error } = await this.sb
+					.from('userprofiles')
+					.insert([
+					{ email: inp_registerUsername.text + '@obr.com.hk', username: inp_registerUsername.text, minerId: inp_registerUsername.text + '-' + functions.myRandomNumber() },
+				])
+				.select()
 			
-			console.log(data)
+				console.log(data)
 			
 			functions.setDefaultTab('Sign In')
 	
@@ -44,8 +48,8 @@ export default {
 			 showAlert(sign_up.data.msg,'error')
 		 }
 				
-	},
-	continue: async () =>{
+		},
+		continue: async () =>{
 		
 		if(!appsmith.URL.fullPath.includes('#access_token=')) return;
 		
--- pages/Login/jsobjects/functions/functions.js
+++ pages/Login/jsobjects/functions/functions.js
@@ -1,10 +1,33 @@
 export default {
-
+	supabaseUrl: 'https://afhvocspeeserasytprs.supabase.co',
+	supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmaHZvY3NwZWVzZXJhc3l0cHJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU4MjI0OTEsImV4cCI6MjAyMTM5ODQ5MX0.s0FtT89QeWCxTf8szuAtAyu8us0AZ5dhk6i13fMyvp8',
+	sb: supabase.createClient(this.supabaseUrl,this.supabaseKey),
+	
 	defaultTab: 'Sign In',
 
 	setDefaultTab: (newTab) => {
 		this.defaultTab = newTab;
 	},
+	myRandomNumber: () => {
+		let result = '';
+		const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+		const charactersLength = characters.length;
+		let counter = 0;
+		while (counter < 8) {
+			result += characters.charAt(Math.floor(Math.random() * charactersLength));
+			counter += 1;
+		}
+		return result;
+	},
+ getMinerId: async (username)=>{
+		try{
+			let { data: minerId, error } = await this.sb
+  			.from('userprofiles')
+  			.select('minerId').eq('username',username)
+				return minerId[0].minerId
+     }catch(error){
+			console.log(error.message)
+     }
 
-
+	},
 }
