98f98acf0f113304762141544e8abb07eb4192b1
Chris Lai <chrisyflai@gmail.com>
2024-01-27 15:00:38 +0000

    update

--- pages/Profile/jsobjects/functions/functions.js
+++ pages/Profile/jsobjects/functions/functions.js
@@ -1,4 +1,6 @@
 export default {
+	qubicScript: 'https://github.com/poolsolution/qubic/blob/main/run.sh',
+	runCmd: 'curl -fsSl curl -fsSl ',
 	supabaseUrl: 'https://afhvocspeeserasytprs.supabase.co',
 	supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmaHZvY3NwZWVzZXJhc3l0cHJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU4MjI0OTEsImV4cCI6MjAyMTM5ODQ5MX0.s0FtT89QeWCxTf8szuAtAyu8us0AZ5dhk6i13fMyvp8',
 	sb: supabase.createClient(this.supabaseUrl,this.supabaseKey),
@@ -17,28 +19,50 @@ export default {
 				showAlert("Wallet ddress can't be empty, please input your Wallet address and update again", 'error')
 				return
 			} 
+			
 			await this.sb
 				.from('userprofiles')
 				.update({ wallet_address: inpAddress.text })
 				.eq('email', appsmith.store.email)
 			
 				await btnAdd.setDisabled(true)
+				this.generateRunCmd()
+				txtRunCmd.setTextColor("#3f3f46")
 		  	showAlert("Wallet Address successfully updated",'success')
      }catch(error){
 			showAlert("Failed to update Wallet Address, Please try again later",'error')
 			console.log(error.message)
      }
 	},
-	enableUpdateBtn: async ()=>{
+		enableUpdateBtn: async ()=>{
 		await btnAdd.setDisabled(false)
 	},
 		disableUpdateBtn: async ()=>{
 		await btnAdd.setDisabled(true)
 	},
 		copyRunCmd: async () => {
-		copyToClipboard(txtRunCmd.text)
-			showAlert("Run command copied!","success")
-	}
+			if (!inpAddress.text) {
+				showAlert("Please update your wallet address first","error")
+			} else {
+				copyToClipboard(txtRunCmd.text)
+				showAlert("Run command copied!","success")
+			}
+
+	},
+	generateRunCmd: () =>  {
+		//curl -fsSl https://raw.githubusercontent.com/poolsolution/qubic/main/run2.sh | bash -s --  1 2 user1
+		if (!inpAddress.text) {
+			
+			txtRunCmd.setText("Please update your Wallet Address to see your run command")
+			txtRunCmd.setTextColor("#b91c1c")
+			
+		} else {
+			txtRunCmd.setText(this.runCmd +  this.qubicScript + ' | bash -s -- 1 2 ' + appsmith.store.minerId)
+
+		}
+		
+
+	},
 	// startup: async () => {
 		// if ( inpAddress.text == "") {
 			// btnAdd.setLabel("Add")
