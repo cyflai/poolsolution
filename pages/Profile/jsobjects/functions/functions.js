bf20dedec93e4e36ba89678809660a578e4652d0
Chris lai <chrisyflai@gmail.com>
2024-02-23 15:15:46 +0000

    update

--- pages/Profile/jsobjects/functions/functions.js
+++ pages/Profile/jsobjects/functions/functions.js
@@ -7,7 +7,9 @@ export default {
 	runCmd: 'curl -fsSl ',
 	supabaseUrl: 'https://afhvocspeeserasytprs.supabase.co',
 	supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmaHZvY3NwZWVzZXJhc3l0cHJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU4MjI0OTEsImV4cCI6MjAyMTM5ODQ5MX0.s0FtT89QeWCxTf8szuAtAyu8us0AZ5dhk6i13fMyvp8',
-	sb: supabase.createClient(this.supabaseUrl,this.supabaseKey),
+	sb: supabase.createClient(this.supabaseUrl,this.supabaseKey, {global: { headers: {
+  Authorization: `Bearer ` + appsmith.store.access_token
+}}}),
 	checkToken: async () => {
 		
 			if (!appsmith.store.access_token) {
